7768049f53017ebb82175ed239938f4f7c635ea0
Vladislav <thelilcheeselad97@gmail.com>
2020-08-13 00:02:12 +0300

    Load site

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$(function(){
+  $('.top-slider').slick({
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: '<button type="button" class="slick-arrow slick-next"><img src="images/next-arrow.svg" alt="next" /></button>',
+    prevArrow: '<button type="button" class="slick-arrow slick-prev"><img src="images/prev-arrow.svg" alt="prev" /></button>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+        }
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          arrows: false
+        }
+      }
+    ]
+  });
+  $('.blockquots__slider').slick({
+    infinite: true,
+    autoplay: true,
+    dots: true,
+    dotsClass: 'bq-dots',
+    initialSlide: 1,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false
+  });
+}); 
